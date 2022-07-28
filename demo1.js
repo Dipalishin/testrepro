868e035b22c823723e3dd5c8fa0eb523248fae7c
dipa <109209362+Dipalishin@users.noreply.github.com>
2022-07-28 14:12:55 +0530

    changes

--- demo1.js
+++ demo1.js
@@ -5,5 +5,12 @@ function saveToLocalStorage(event)
     const emailId=event.target.emailId.value;
     localStorage.setItem('name',name);
     localStorage.setItem('emailId',emailId);
-    //console.log(localStorage.getItem('username')) ;   
-    }
+    //console.log(localStorage.getItem('username'));
+    let obj={
+    name:name,
+    emailId:emailId    
+          }
+  localStorage.setItem("Userdetails",JSON.stringify(obj));
+   let obj_deserilized=JSON.parse(localStorage.getItem(obj));
+  console.log(obj_deserilized);
+}
