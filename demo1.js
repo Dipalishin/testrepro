114e2f87881555a2f2d8ddd23fdafd07653fef50
dipa <109209362+Dipalishin@users.noreply.github.com>
2022-07-29 14:52:54 +0530

    edit and delete function

--- demo1.js
+++ demo1.js
@@ -3,14 +3,59 @@ function saveToLocalStorage(event)
     event.preventDefault();
     const name=event.target.username.value;
     const emailId=event.target.emailId.value;
-    localStorage.setItem('name',name);
-    localStorage.setItem('emailId',emailId);
-    //console.log(localStorage.getItem('username'));
     let obj={
     name:name,
     emailId:emailId    
           }
-  localStorage.setItem("Userdetails",JSON.stringify(obj));
-   let obj_deserilized=JSON.parse(localStorage.getItem(obj));
-  console.log(obj_deserilized);
+  localStorage.setItem(obj.emailId,JSON.stringify(obj));
+  showNewUserOnScreen(obj);
 }
+window.addEventListener("DOMContentLoaded", () => {
+    const localStorageObj = localStorage;
+    const localstoragekeys  = Object.keys(localStorageObj)
+
+    for(var i =0; i< localstoragekeys.length; i++){
+        const key = localstoragekeys[i]
+        const userDetailsString = localStorageObj[key];
+        const userDetailsObj = JSON.parse(userDetailsString);
+        showNewUserOnScreen(userDetailsObj);
+    }
+})
+function showNewUserOnScreen(user)
+{
+    document.getElementById("emailId").value='';
+    document.getElementById("username").value='';
+
+    if(localStorage.getItem(user.emailId)!==null)
+    {
+        removeUserFromScreen(user.emailId);
+    }
+    const parentNode = document.getElementById('listOfUsers');
+                const childHTML = `<li id=${user.emailId}> ${user.name} - ${user.emailId}
+                                        <button onclick=deleteUser('${user.emailId}')> Delete User </button>
+                                        <button onclick=editUser('${user.emailId}','${user.name}')> Edit User </button>
+                                        </li>`
+parentNode.innerHTML=parentNode.innerHTML+childHTML;
+}
+function deleteUser(emailId){
+    console.log(emailId)
+    localStorage.removeItem(emailId);
+    removeUserFromScreen(emailId);
+
+}
+
+function removeUserFromScreen(emailId){
+    const parentNode = document.getElementById('listOfUsers');
+    const childNodeToBeDeleted = document.getElementById(emailId);
+if(childNodeToBeDeleted){
+    parentNode.removeChild(childNodeToBeDeleted)
+}
+}
+function editUser(emailId,name)
+{
+    document.getElementById("emailId").value=emailId;
+    document.getElementById("username").value=name;
+deleteUser(emailId);
+
+}
+
